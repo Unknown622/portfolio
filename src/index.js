b49219ae3c646ff071c87b85618cf2c5b880b0d8
Vincent <57076241+Unknown622@users.noreply.github.com>
2022-07-24 14:00:24 -0500

    Update files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import reportWebVitals from './reportWebVitals'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./Pages/Home"
+import {createTheme, ThemeProvider} from "@mui/material"
+import 'animate.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+// Theme from: https://www.color-hex.com/color-palette/1294
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#005b96",
+        },
+        secondary: {
+            main: "#03396c",
+        },
+    },
+})
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log)
